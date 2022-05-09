6e2020358df4dd982e03333ae541af7bfbfda50d
Sirbickle <lima.ll62@gmail.com>
2022-05-09 16:05:13 -0300

    Exercicio de calculos

---
+++ Projetos Java/exercicio 1/dio exercico de calculos.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication11;
+
+/**
+ *
+ * @author limal
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double n1, n2, resultado;
+        n1 = 5 ;
+        n2 = 5;
+                
+        resultado = n1 + n2;
+        System.out.println ("A soma de " + n1 + " + " + n2 + " é de " + resultado );
+        
+        resultado = n1 - n2;
+        System.out.println ("A subtração de " + n1 + " - " + n2 + " é de " + resultado);
+        
+        resultado = n1 * n2;
+        System.out.println ("A multiplicação de " + n1 + " * " + n2 +  " é de " + resultado);
+        
+        resultado = n1 / n2;
+        System.out.println ("A divisão de " + n1 + " / " + n2 + " é de é " + resultado);
+         
+    }
+    
+}
